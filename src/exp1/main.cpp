32d35a04840b39ed170196b771a873ae6dd4bc8b
Librazy <librazy@foxmail.com>
2017-03-02 21:50:23 +0800

    -

--- src/exp1/main.cpp
+++ src/exp1/main.cpp
@@ -1,5 +1,4 @@
 #include<cstdio>
-#include <GL/glew.h>
 #include <GL/freeglut.h>
 
 int nWindow;
@@ -9,49 +8,35 @@ float cR = 1.0f;
 float cG = 0.0f;
 float cB = 0.0f;
 
-void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY );
-void Redisplay();
-void Reshape(int width, int height);
-
-void UnhideTimer(int window)
+void SampleKeyboard(unsigned char cChar, int nMouseX, int nMouseY)
 {
-    glutSetWindow(window);
-    glutShowWindow();
-}
+	switch (cChar)
+	{
+	case 27:
+		glutLeaveMainLoop();
+		break;
 
-void ChangeTitleTimer(int unused)
-{
-    glutSetIconTitle("new icon title");
-    glutSetWindowTitle("new test title");
-}
-
-void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY )
-{
-    switch (cChar)
-    {
-    case 27:
-        glutLeaveMainLoop();
-        break;
-        
-    case 'c':
-    case 'C':
-        if(cR != 0){
-            cR = 0;
-            cG = 1.0f;
-        }else if(cG != 0){
-            cG = 0;
-            cB = 1.0f;
-        }else{
-            cB = 0;
-            cR = 1.0f;
-        }
-        break;
+	case 'c':
+	case 'C':
+		if (cR != 0) {
+			cR = 0;
+			cG = 1.0f;
+		}
+		else if (cG != 0) {
+			cG = 0;
+			cB = 1.0f;
+		}
+		else {
+			cB = 0;
+			cR = 1.0f;
+		}
+		break;
 
 	case 'r':
 	case 'R':
-			cR = 1.0f;
-			cG = 0;
-			cB = 0;
+		cR = 1.0f;
+		cG = 0;
+		cB = 0;
 		break;
 
 	case 'g':
@@ -68,68 +53,63 @@ void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY )
 		cB = 1.0f;
 		break;
 
-    case 'f':
-    case 'F':
-        glutFullScreenToggle();
-        break;
+	case 'f':
+	case 'F':
+		glutFullScreenToggle();
+		break;
 
-    case 'h':
-    case 'H':
-        glutSetWindow(nWindow);
-        glutIconifyWindow();
-        break;
-    default:
-        break;
-    }
-}
-
-void Idle(void)
-{
-    glutPostRedisplay();
+	case 'h':
+	case 'H':
+		glutSetWindow(nWindow);
+		glutIconifyWindow();
+		break;
+	default:
+		break;
+	}
 }
 
 void Reshape(int width, int height)
 {
-    glViewport(0,0,width,height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0,width,0,height);
-    glutSetWindow(nWindow);
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, width, 0, height);
+	glutSetWindow(nWindow);
 }
 
 void Redisplay()
 {
-    int win = glutGetWindow();
-    int viewport[4];
-    glClear(GL_COLOR_BUFFER_BIT); 
-    glClearColor(cR, cG, cB, 0.f);
-    glColor3f(cG,cB,cR);
-    glGetIntegerv(GL_VIEWPORT, viewport);
-    glRasterPos2i(2, -glutBitmapHeight(GLUT_BITMAP_9_BY_15)+3+viewport[3]);
-    glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"press f/h/c");
-    
-    glutSwapBuffers();
-    glutPostWindowRedisplay(win);
+	int win = glutGetWindow();
+	int viewport[4];
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(cR, cG, cB, 0.f);
+	glColor3f(cG, cB, cR);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+	glRasterPos2i(2, -glutBitmapHeight(GLUT_BITMAP_9_BY_15) + 3 + viewport[3]);
+	glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"press f/h/c");
+
+	glutSwapBuffers();
+	glutPostWindowRedisplay(win);
 }
 
 
 int main(int argc, char* argv[])
 {
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
-    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 
-    glutInitWindowPosition(150,250);
-    glutInitWindowSize(200,200);
+	glutInitWindowPosition(150, 250);
+	glutInitWindowSize(200, 200);
 
-    nWindow = glutCreateWindow("homework 1");
-    glutSetIconTitle("homework 1");
+	nWindow = glutCreateWindow("homework 1");
+	glutSetIconTitle("homework 1");
 
-    glutKeyboardFunc( SampleKeyboard );
-    glutDisplayFunc( Redisplay );
-    glutReshapeFunc( Reshape );
+	glutKeyboardFunc(SampleKeyboard);
+	glutDisplayFunc(Redisplay);
+	glutReshapeFunc(Reshape);
 
-    glutMainLoop();
+	glutMainLoop();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- src/exp2/main.cpp
+++ src/exp2/main.cpp
@@ -1,56 +1,62 @@
 #include<cstdio>
-#include <GL/freeglut.h>
+#include<GLTools.h>
+#include<GLShaderManager.h>
+#include<GL/freeglut.h>
 
 int nWindow;
 int nLoopMain = 0;
-GLboolean bChildPosDone = GL_FALSE, bChildSizeDone = GL_FALSE;
+GLShaderManager shaderManager;
 float cR = 1.0f;
 float cG = 0.0f;
 float cB = 0.0f;
-
-void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY );
-void Redisplay();
-void Reshape(int width, int height);
-
-void UnhideTimer(int window)
-{
-    glutSetWindow(window);
-    glutShowWindow();
+float x = 0.0f;
+float y = 0.0f;
+void SampleSpecial(int c, int mx, int my) {
+	switch (c)
+	{
+	case GLUT_KEY_UP:
+		y += 0.01;
+		break;
+	case GLUT_KEY_DOWN:
+		y -= 0.01;
+		break;
+	case GLUT_KEY_LEFT:
+		x -= 0.01;
+		break;
+	case GLUT_KEY_RIGHT:
+		x += 0.01;
+		break;
+	}
 }
-
-void ChangeTitleTimer(int unused)
+void SampleKeyboard(unsigned char cChar, int nMouseX, int nMouseY)
 {
-    glutSetIconTitle("new icon title");
-    glutSetWindowTitle("new test title");
-}
+	switch (cChar)
+	{
+	case 27:
+		glutLeaveMainLoop();
+		break;
 
-void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY )
-{
-    switch (cChar)
-    {
-    case 27:
-        glutLeaveMainLoop();
-        break;
-        
-    case 'c':
-    case 'C':
-        if(cR != 0){
-            cR = 0;
-            cG = 1.0f;
-        }else if(cG != 0){
-            cG = 0;
-            cB = 1.0f;
-        }else{
-            cB = 0;
-            cR = 1.0f;
-        }
-        break;
+	case 'c':
+	case 'C':
+		if (cR != 0) {
+			cR = 0;
+			cG = 1.0f;
+		}
+		else if (cG != 0) {
+			cG = 0;
+			cB = 1.0f;
+		}
+		else {
+			cB = 0;
+			cR = 1.0f;
+		}
+		break;
 
 	case 'r':
 	case 'R':
-			cR = 1.0f;
-			cG = 0;
-			cB = 0;
+		cR = 1.0f;
+		cG = 0;
+		cB = 0;
 		break;
 
 	case 'g':
@@ -67,68 +73,87 @@ void SampleKeyboard( unsigned char cChar, int nMouseX, int nMouseY )
 		cB = 1.0f;
 		break;
 
-    case 'f':
-    case 'F':
-        glutFullScreenToggle();
-        break;
+	case 'f':
+	case 'F':
+		glutFullScreenToggle();
+		break;
 
-    case 'h':
-    case 'H':
-        glutSetWindow(nWindow);
-        glutIconifyWindow();
-        break;
-    default:
-        break;
-    }
-}
-
-void Idle(void)
-{
-    glutPostRedisplay();
+	case 'h':
+	case 'H':
+		glutSetWindow(nWindow);
+		glutIconifyWindow();
+		break;
+	default:
+		break;
+	}
 }
 
 void Reshape(int width, int height)
 {
-    glViewport(0,0,width,height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0,width,0,height);
-    glutSetWindow(nWindow);
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, width, 0, height);
+	glutSetWindow(nWindow);
 }
 
 void Redisplay()
 {
-    int win = glutGetWindow();
-    int viewport[4];
-    glClear(GL_COLOR_BUFFER_BIT); 
-    glClearColor(cR, cG, cB, 0.f);
-    glColor3f(cG,cB,cR);
-    glGetIntegerv(GL_VIEWPORT, viewport);
-    glRasterPos2i(2, -glutBitmapHeight(GLUT_BITMAP_9_BY_15)+3+viewport[3]);
-    glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"press f/h/c");
-    
-    glutSwapBuffers();
-    glutPostWindowRedisplay(win);
+	int win = glutGetWindow();
+	int viewport[4];
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(cR, cG, cB, 0.0f);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+	glRasterPos2i(2, -glutBitmapHeight(GLUT_BITMAP_9_BY_15) + 3 + viewport[3]);
+
+	GLBatch tri;
+	tri.Begin(GL_TRIANGLES, 3);
+
+	GLfloat vert[] = {
+		x-0.5f, y     , 0.0f,
+		x+0.5f, y     , 0.0f,
+		x     , y+0.5f, 0.0f,
+	};
+	GLfloat color[] = {
+		cG, cB, cR
+	};
+	GLfloat color2[] = {
+		cB, cR, cG
+	};
+	tri.CopyVertexData3f(vert);
+	tri.End();
+
+	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, color);
+	tri.Draw();
+	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, color2);
+	glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"press f/h/c");
+
+	glutSwapBuffers();
+	glutPostWindowRedisplay(win);
 }
 
 
 int main(int argc, char* argv[])
 {
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
-    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 
-    glutInitWindowPosition(150,250);
-    glutInitWindowSize(200,200);
+	nWindow = glutCreateWindow("homework 1");
+	glutSetIconTitle("homework 1");
 
-    nWindow = glutCreateWindow("homework 1");
-    glutSetIconTitle("homework 1");
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		fprintf(stderr, "GLEW error : %s \n", glewGetErrorString(err));
+		return 1;
+	}
+	shaderManager.InitializeStockShaders();
+	glutKeyboardFunc(SampleKeyboard);
+	glutSpecialFunc(SampleSpecial);
+	glutDisplayFunc(Redisplay);
+	glutReshapeFunc(Reshape);
 
-    glutKeyboardFunc( SampleKeyboard );
-    glutDisplayFunc( Redisplay );
-    glutReshapeFunc( Reshape );
+	glutMainLoop();
 
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
